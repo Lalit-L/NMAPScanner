ffd34605a8408e46ec008d694251c0aca27b34b3
Lalit-L <lalitlakamsani@gmail.com>
2024-05-31 23:10:30 -0700

    Added a SYN ACK scanner that returns the open ports on the given address, if the address is up or down, and performs a SYN ACK scan on the address

--- nmapScanner.py
+++ nmapScanner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import nmap
 
 scanner = nmap.PortScanner()
@@ -5,5 +7,26 @@ scanner = nmap.PortScanner()
 print("Hello! This is a test version of an NMAP Scanner in Python!")
 print("-" * 65)
 
+# Collects the IP Address the user wants to scan
 address = input("Please enter the address you wish to scan: ")
-print("The address you've entered is: " + address)
+print("The address you've entered is: " + address + "\n")
+
+print("A SYN ACK scan will be performed on the address given\n")
+
+# Scans the version of NMAP
+print("NMAP Version: ", scanner.nmap_version())
+
+# Scans from ports 1 to 10000 on the given address, also verboses the output and performs a SYN ACK scan
+scanner.scan(address, '1-10000', '-v -sS')
+
+# Prints the info gained from the previous line
+print(scanner.scaninfo())
+
+# Prints whether or not the Address given is up or down
+print("IP Status: ", scanner[address].state())
+
+# Prints the protocol that is being scanned
+print(scanner[address].all_protocols())
+
+# Prints the open ports
+print("Open ports: ", scanner[address]['tcp'].keys())
