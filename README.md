e06fc882f6e7e02834ed1dea15ec6d41410ab4de
Lalit-L <lalitlakamsani@gmail.com>
2024-06-03 22:02:53 -0700

    FINAL COMMIT - Added a Comprehensive Scan and allowed the user to enter the range of ports to be scanned

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ Thoughts: It was pretty interesting to learn about the different types of scans 
 Edit 3: Added a UDP scan as well. This will scan for open UDP Ports on the address entered, which the user can use if they are opting for a faster scan. Due to there not being acknowledgement of a receipt, it is a less reliable scan method, but it is also a little more stealthy compared to the SYN ACK scan I implemented before
 
 Thoughts: It was fun learning about a different scan method, considering I thought there would be only so many ways to scan for ports and openings on an address. The speed of the UDP scan is quite nice, but I can see how the reliability is lacking. The stealth aspect of the scan is cool though, as some firewalls and systems might not log its activity as much. Overall it wasn't difficult to implement and was enjoyable
+
+---------------------------------------------------------------------------------------
+
+Edit 4: FINAL EDIT. Added a comprehensive scan and a check to make sure that a correct option is chosen. Also added user input for the ports to be scanned using a range. Cleaned up other comments while adding some to the Comprehensive scan portion
+
+Thoughts: FINAL THOUGHTS. This was fun! It felt really gratifying to finish a project like this, because I had never really done something like this. I did the port scanner, which was a nice buildup to this because of how it functions with each scan, but this was still super informative. The Comprehensive Scan does take a while to fully scan, but it does make sense given the amount it has to go through. Overall, this was a very rewarding project.
--- nmapScanner.py
+++ nmapScanner.py
@@ -15,9 +15,10 @@ print("-" * 65)
 address = input("Please enter the address you wish to scan: ")
 print("The address you've entered is: " + address + "\n")
 
+numPorts = input("Please enter the ports you would like to scan (Enter a range such as 1-1000):\n")
 
 choice = input("Please enter the number of the type of scan you would like to run on the address: \n" +
-		" 1) UDP Scan\n 2) SYN ACK Scan\n")
+		" 1) UDP Scan\n 2) SYN ACK Scan\n 3) Comprehensive Scan\n")
 
 if choice == '1':
 	print("\nA UDP scan will be performed on the address given\n")
@@ -27,7 +28,7 @@ if choice == '1':
 	print("NMAP Version: ", scanner.nmap_version())
 
 	# Scans from ports 1 to 10000 on the given address, also scans for UDP ports
-	scanner.scan(address, '1-10000', '-v -sU')
+	scanner.scan(address, f'{numPorts}', '-v -sU')
 
 	# Prints the info gained from the previous line
 	print(scanner.scaninfo())
@@ -48,7 +49,7 @@ elif choice == '2':
 	print("NMAP Version: ", scanner.nmap_version())
 
 	# Scans from ports 1 to 10000 on the given address, also verboses the output and performs a SYN ACK scan
-	scanner.scan(address, '1-10000', '-v -sS')
+	scanner.scan(address, f'{numPorts}', '-v -sS')
 
 	# Prints the info gained from the previous line
 	print(scanner.scaninfo())
@@ -61,3 +62,26 @@ elif choice == '2':
 
 	# Prints the open TCP ports
 	print("Open ports: ", scanner[address]['tcp'].keys())
+elif choice == '3':
+	print("\nA Comprehensive scan will be performed on the address given\n")
+	print("(Note that this may take some time)\n")
+
+	# Scans the version of NMAP
+	print("NMAP Version: ", scanner.nmap_version())
+
+	# Scans from ports 1 to 10000 on the given address, also verboses the output and performs a SYN scan, as well as service fingerprinting, checks default scans, and an aggressive scan, as well OS detection
+	scanner.scan(address, f'{numPorts}', '-v -sS -sV -sC -A -O')
+
+	# Prints the info gained from the previous line
+	print(scanner.scaninfo())
+
+	# Prints whether or not the Address given is up or down
+	print("IP Status: ", scanner[address].state())
+
+	# Prints the protocol that is being scanned
+	print(scanner[address].all_protocols())
+
+	# Prints the open TCP ports
+	print("Open ports: ", scanner[address]['tcp'].keys())
+else:
+	print("Please enter a number either 1,2, or 3 to select the scan option you would like to use")
